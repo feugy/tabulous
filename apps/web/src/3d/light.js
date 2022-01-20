c39b92f4f8ec387d74d96baf8ce3466942377f9d
feugy <damien.feugas@gmail.com>
2022-01-20 22:37:23 +0100

    feat(web): ambient lights for better textures

--- apps/web/src/3d/light.js
+++ apps/web/src/3d/light.js
@@ -1,6 +1,7 @@
 // mandatory side effect
 import '@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent'
 import { DirectionalLight } from '@babylonjs/core/Lights/directionalLight'
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight'
 import { ShadowGenerator } from '@babylonjs/core/Lights/Shadows/shadowGenerator'
 import { Color3 } from '@babylonjs/core/Maths/math.color'
 import { Vector3 } from '@babylonjs/core/Maths/math.vector'
@@ -20,6 +21,8 @@ export function createLight() {
   const light = new DirectionalLight('sun', new Vector3(0, -10, 0))
   light.intensity = 0.8
   light.specular = Color3.Black()
+  const ambientLight = new HemisphericLight('ambient', new Vector3(0, -1, 0))
+  ambientLight.intensity = 0.8
 
   const shadowGenerator = new ShadowGenerator(1024, light)
   shadowGenerator.usePercentageCloserFiltering = true
@@ -29,5 +32,5 @@ export function createLight() {
     .onNewMeshAddedObservable.add(mesh =>
       shadowGenerator.getShadowMap().renderList.push(mesh)
     )
-  return { light, shadowGenerator }
+  return { light, ambientLight, shadowGenerator }
 }
