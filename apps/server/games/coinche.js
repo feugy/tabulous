00a89b3902f9479b04a7bbe8fa7dccb8628b7621
feugy <damien.feugas@gmail.com>
2022-02-24 08:15:23 +0100

    feat(coinche): introduces coinche game

---
+++ apps/server/games/coinche.js
@@ -0,0 +1,47 @@
+/**
+ * 8 cards of each suit: spades, diamonds, clubs, hearts.
+ * @type {import('../src/services/games').Mesh[]}
+ */
+export const meshes = []
+
+const suits = ['spades', 'diamonds', 'clubs', 'hearts']
+
+const depth = 4.25
+const width = 3
+const height = 0.01
+
+for (const suit of suits) {
+  for (let index = 1; index <= 13; index++) {
+    if (index === 1 || index >= 7) {
+      meshes.push({
+        shape: 'card',
+        id: `${suit}-${index}`,
+        texture: `images/french-suited-cards/${suit}-${index}.ktx2`,
+        x: 0,
+        y: 0,
+        z: 0,
+        width,
+        height,
+        depth,
+        // use all defaults
+        movable: {},
+        stackable: {},
+        drawable: {},
+        flippable: { isFlipped: true },
+        rotable: {}
+      })
+    }
+  }
+}
+
+/**
+ * Bags to randomize: one for all cards
+ * @type {Map<string, string[]>}
+ */
+export const bags = new Map([['cards', meshes.map(({ id }) => id)]])
+
+/**
+ * Pre-defined slots: one for all cards
+ * @type {import('../src/services/utils').Slot[]}
+ */
+export const slots = [{ bagId: 'cards' }]
