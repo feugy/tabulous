d7eb09105e7feb47e33b343b3aa02178d8243dea
feugy <damien.feugas@gmail.com>
2022-04-21 08:51:26 +0200

    feat(web): retrieves turn credentials during authentication

--- apps/web/tests/stores/players.test.js
+++ apps/web/tests/stores/players.test.js
@@ -1,11 +1,12 @@
 import { faker } from '@faker-js/faker'
 import * as graphQL from '../../src/graphql'
 import {
-  currentPlayer,
+  currentPlayer as currentPlayer$,
   logIn,
   logOut,
   recoverSession,
-  searchPlayers
+  searchPlayers,
+  turnCredentials as turnCredentials$
 } from '../../src/stores/players'
 import { runMutation, runQuery } from '../../src/stores/graphql-client'
 import { mockLogger } from '../utils'
@@ -25,31 +26,41 @@ describe('searchPlayers()', () => {
 
 describe('given a subscription to current player', () => {
   const logger = mockLogger('players')
-  let subscription
-  let loggedPlayer
+  let subscriptions
+  const playerChanged = jest.fn()
+  const turnCredentialsChanged = jest.fn()
   const username = faker.name.firstName()
   const password = faker.internet.password()
   const player = { id: faker.datatype.uuid(), username }
+  const turnCredentials = {
+    username: faker.lorem.words(),
+    credentials: faker.datatype.uuid()
+  }
 
   beforeAll(() => {
-    subscription = currentPlayer.subscribe(player => (loggedPlayer = player))
+    subscriptions = [
+      currentPlayer$.subscribe(playerChanged),
+      turnCredentials$.subscribe(turnCredentialsChanged)
+    ]
   })
 
   beforeEach(jest.resetAllMocks)
 
-  afterAll(() => subscription?.unsubscribe())
+  afterAll(() => subscriptions.forEach(sub => sub.unsubscribe()))
 
   describe('logIn()', () => {
-    it('sets current player on success', async () => {
-      runMutation.mockResolvedValueOnce(player)
-      expect(loggedPlayer).not.toEqual(player)
+    it('sets current player and turn credentials on success', async () => {
+      runMutation.mockResolvedValueOnce({ player, turnCredentials })
       expect(await logIn(username, password)).toEqual(player)
       expect(runMutation).toHaveBeenCalledWith(graphQL.logIn, {
         username,
         password
       })
       expect(runMutation).toHaveBeenCalledTimes(1)
-      expect(loggedPlayer).toEqual(player)
+      expect(playerChanged).toHaveBeenCalledWith(player)
+      expect(playerChanged).toHaveBeenCalledTimes(1)
+      expect(turnCredentialsChanged).toHaveBeenCalledWith(turnCredentials)
+      expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
     })
 
     it('reset current player on failure', async () => {
@@ -60,23 +71,30 @@ describe('given a subscription to current player', () => {
         password
       })
       expect(runMutation).toHaveBeenCalledTimes(1)
-      expect(loggedPlayer).toBeNull()
+      expect(playerChanged).toHaveBeenCalledWith(null)
+      expect(playerChanged).toHaveBeenCalledTimes(1)
+      expect(turnCredentialsChanged).toHaveBeenCalledWith(null)
+      expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
     })
   })
 
   describe('given an authenticated player', () => {
     beforeEach(async () => {
-      runMutation.mockResolvedValueOnce(player)
+      runMutation.mockResolvedValueOnce({ player, turnCredentials })
       expect(await logIn(username, password)).toEqual(player)
-      expect(loggedPlayer).toEqual(player)
       runMutation.mockReset()
+      playerChanged.mockReset()
+      turnCredentialsChanged.mockReset()
     })
 
     describe('logOut()', () => {
       it('clears current player', async () => {
         await logOut()
-        expect(loggedPlayer).toBeNull()
         expect(runMutation).not.toHaveBeenCalled()
+        expect(playerChanged).toHaveBeenCalledWith(null)
+        expect(playerChanged).toHaveBeenCalledTimes(1)
+        expect(turnCredentialsChanged).toHaveBeenCalledWith(null)
+        expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
       })
     })
 
@@ -84,30 +102,48 @@ describe('given a subscription to current player', () => {
       it('logs player out on missing data', async () => {
         sessionStorage.clear()
         expect(await recoverSession()).toBeNull()
-        expect(loggedPlayer).toBeNull()
+        expect(playerChanged).toHaveBeenCalledWith(null)
+        expect(playerChanged).toHaveBeenCalledTimes(1)
+        expect(turnCredentialsChanged).toHaveBeenCalledWith(null)
+        expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
         expect(logger.warn).not.toHaveBeenCalled()
       })
 
       it('logs player out on invalid data', async () => {
-        sessionStorage.setItem('player', '{"invalid": true')
+        sessionStorage.setItem('session', '{"invalid": true')
         expect(await recoverSession()).toBeNull()
-        expect(loggedPlayer).toBeNull()
+        expect(playerChanged).toHaveBeenCalledWith(null)
+        expect(playerChanged).toHaveBeenCalledTimes(1)
+        expect(turnCredentialsChanged).toHaveBeenCalledWith(null)
+        expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
         expect(logger.warn).toHaveBeenCalledTimes(1)
       })
 
       it('logs player out on outdated data', async () => {
-        sessionStorage.setItem('player', JSON.stringify(player))
+        sessionStorage.setItem(
+          'session',
+          JSON.stringify({ player, turnCredentials })
+        )
         runQuery.mockRejectedValueOnce(new Error('outdated!'))
         expect(await recoverSession()).toBeNull()
-        expect(loggedPlayer).toBeNull()
+        expect(playerChanged).toHaveBeenCalledWith(null)
+        expect(playerChanged).toHaveBeenCalledTimes(1)
+        expect(turnCredentialsChanged).toHaveBeenCalledWith(null)
+        expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
         expect(logger.warn).toHaveBeenCalledTimes(1)
       })
 
       it('authenticates player with valid data', async () => {
-        sessionStorage.setItem('player', JSON.stringify(player))
+        sessionStorage.setItem(
+          'session',
+          JSON.stringify({ player, turnCredentials })
+        )
         runQuery.mockResolvedValueOnce(player)
         expect(await recoverSession()).toEqual(player)
-        expect(loggedPlayer).toEqual(player)
+        expect(playerChanged).toHaveBeenCalledWith(player)
+        expect(playerChanged).toHaveBeenCalledTimes(1)
+        expect(turnCredentialsChanged).toHaveBeenCalledWith(turnCredentials)
+        expect(turnCredentialsChanged).toHaveBeenCalledTimes(1)
         expect(logger.warn).not.toHaveBeenCalled()
       })
     })
