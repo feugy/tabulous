cec25c6db55fc5e6c886fde907c1e911ba04d884
feugy <damien.feugas@gmail.com>
2022-05-16 22:12:47 +0200

    feat(coinche): uses a table texture
    Also refactors logic to align with latest standard

--- apps/games/assets/coinche/logic/build.js
+++ apps/games/assets/coinche/logic/build.js
@@ -1,51 +1,48 @@
+const bagId = 'cards'
+const suits = ['spades', 'diamonds', 'clubs', 'hearts']
+const sizes = { card: { depth: 4.25, width: 3, height: 0.01 } }
+const counts = { suit: 13 }
+
 export function build() {
   /**
    * 8 cards of each suit: spades, diamonds, clubs, hearts.
    * @type {import('@tabulous/server/src/services/games').Mesh[]}
    */
-  const meshes = []
-
-  const suits = ['spades', 'diamonds', 'clubs', 'hearts']
-
-  const depth = 4.25
-  const width = 3
-  const height = 0.01
-
-  for (const suit of suits) {
-    for (let index = 1; index <= 13; index++) {
-      if (index === 1 || index >= 7) {
-        meshes.push({
-          shape: 'card',
-          id: `${suit}-${index}`,
-          texture: `/games/french-suited-card/textures/${suit}-${index}.ktx2`,
-          x: 0,
-          y: 0,
-          z: 0,
-          width,
-          height,
-          depth,
-          // use all defaults
-          movable: {},
-          stackable: {},
-          drawable: {},
-          flippable: { isFlipped: true },
-          rotable: {}
-        })
-      }
-    }
-  }
+  const meshes = buildCards()
 
   /**
    * Bags to randomize: one for all cards
    * @type {Map<string, string[]>}
    */
-  const bags = new Map([['cards', meshes.map(({ id }) => id)]])
+  const bags = new Map([[bagId, meshes.map(({ id }) => id)]])
 
   /**
    * Pre-defined slots: one for all cards
    * @type {import('@tabulous/server/src/services/utils').Slot[]}
    */
-  const slots = [{ bagId: 'cards' }]
+  const slots = [{ bagId }]
 
   return { meshes, bags, slots }
+}
+
+function buildCards() {
+  const meshes = []
+  for (const suit of suits) {
+    for (let index = 1; index <= counts.suit; index++) {
+      if (index === 1 || index >= 7) {
+        meshes.push({
+          shape: 'card',
+          id: `${suit}-${index}`,
+          texture: `/games/french-suited-card/textures/${suit}-${index}.ktx2`,
+          ...sizes.card,
+          flippable: { isFlipped: true },
+          movable: {},
+          stackable: {},
+          drawable: {},
+          rotable: {}
+        })
+      }
+    }
+  }
+  return meshes
 }
