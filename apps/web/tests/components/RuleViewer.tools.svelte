eb055198dc467cfd1098b3c5734643f6500c8ca5
feugy <damien.feugas@gmail.com>
2022-08-27 22:19:26 +0200

    tests(web): adds tools for login and home pages

--- apps/web/tests/components/RuleViewer.tools.svelte
+++ apps/web/tests/components/RuleViewer.tools.svelte
@@ -3,19 +3,19 @@
   import { RuleViewer } from '../../src/components'
 </script>
 
-<div class="rule-viewer">
-  <Tool
-    component={RuleViewer}
-    name="Components/Rule Viewer"
-    props={{ game: 'splendor', lastPage: 3 }}
-    events={['change']}
-    layout="centered"
-    let:props
-    let:handleEvent
-  >
+<Tool
+  component={RuleViewer}
+  name="Components/Rule Viewer"
+  props={{ game: 'splendor', lastPage: 3 }}
+  events={['change']}
+  layout="centered"
+  let:props
+  let:handleEvent
+>
+  <div class="rule-viewer">
     <RuleViewer {...props} on:change={handleEvent} />
-  </Tool>
-</div>
+  </div>
+</Tool>
 
 <style lang="postcss">
   .rule-viewer {
---
+++ apps/web/tests/routes/login/+page.tools.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { Tool, ToolBox } from '@atelier-wb/svelte'
+  import LoginPage from '../../../src/routes/login/+page.svelte'
+</script>
+
+<ToolBox component={LoginPage} name="Routes/login">
+  <Tool name="Default" />
+  <Tool name="With errors" props={{ errors: [{ message: 'Forbidden' }] }} />
+</ToolBox>
