736fb582950b486313bed66de1bad8c84a2712f0
feugy <damien.feugas@gmail.com>
2022-11-03 18:34:21 +0100

    feat(cli): enhances player details with game statistics

--- apps/cli/tests/commands/show-player.test.js
+++ apps/cli/tests/commands/show-player.test.js
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 import stripAnsi from 'strip-ansi'
-import { it } from 'vitest'
+import { describe, it } from 'vitest'
 import { applyFormaters } from '../../src/util/formaters.js'
 import { signToken } from '../../src/util/jwt.js'
 
@@ -13,11 +13,16 @@ vi.mock('../../src/util/graphql-client.js', () => ({
     .mockReturnValue({ query: mockQuery, mutation: mockMutation })
 }))
 
-describe('User revokes command', () => {
+describe('Show player command', () => {
   let showPlayer
   const adminUserId = faker.datatype.uuid()
   const jwtKey = faker.datatype.uuid()
   const player = {
+    id: faker.datatype.uuid(),
+    username: faker.name.fullName(),
+    email: faker.internet.email()
+  }
+  const player2 = {
     id: faker.datatype.uuid(),
     username: faker.name.fullName(),
     email: faker.internet.email()
@@ -65,60 +70,72 @@ describe('User revokes command', () => {
     expect(mockQuery).toHaveBeenCalledTimes(1)
   })
 
-  it('displays found player', async () => {
-    mockQuery.mockResolvedValueOnce({ searchPlayers: [player] })
-    const result = await showPlayer(['-u', player.username])
-    expect(result).toMatchObject(player)
-    const rawOutput = stripAnsi(applyFormaters(result).join('\n'))
-    expect(rawOutput).toContain(`id:             ${player.id}`)
-    expect(rawOutput).toContain(`username:       ${player.username}`)
-    expect(rawOutput).toContain(`email:          ${player.email}`)
-    expect(rawOutput).toContain(`provider:       manual`)
-    expect(rawOutput).toContain(`is playing:     false`)
-    expect(rawOutput).toContain(`terms accepted: false`)
-  })
+  describe('given some games', () => {
+    const games = [
+      { id: faker.datatype.uuid(), kind: 'klondike', players: [player] },
+      { id: faker.datatype.uuid(), kind: 'klondike', players: [player] },
+      {
+        id: faker.datatype.uuid(),
+        kind: 'klondike',
+        players: [player, player2]
+      },
+      {
+        id: faker.datatype.uuid(),
+        kind: 'klondike',
+        players: [player2, player]
+      }
+    ]
 
-  it('handles boolean values', async () => {
-    mockQuery.mockResolvedValueOnce({
-      searchPlayers: [{ ...player, termsAccepted: false, playing: true }]
+    it('displays found player', async () => {
+      mockQuery
+        .mockResolvedValueOnce({ searchPlayers: [player] })
+        .mockResolvedValueOnce({ listGames: [] })
+      const result = await showPlayer(['-u', player.username])
+      expect(result).toMatchObject(player)
+      const rawOutput = stripAnsi(applyFormaters(result).join('\n'))
+      expect(rawOutput).toContain(`id:             ${player.id}`)
+      expect(rawOutput).toContain(`username:       ${player.username}`)
+      expect(rawOutput).toContain(`email:          ${player.email}`)
+      expect(rawOutput).toContain(`provider:       manual`)
+      expect(rawOutput).toContain(`is playing:     false`)
+      expect(rawOutput).toContain(`terms accepted: false`)
+      expect(rawOutput).toContain(`total games:    0`)
     })
-    const rawOutput = stripAnsi(
-      applyFormaters(await showPlayer(['-u', player.username])).join('\n')
-    )
-    expect(rawOutput).toContain(`is playing:     true`)
-    expect(rawOutput).toContain(`terms accepted: false`)
-  })
 
-  it('handles provider', async () => {
-    mockQuery.mockResolvedValueOnce({
-      searchPlayers: [{ ...player, provider: 'github' }]
+    it('count total, owned, and invited games', async () => {
+      mockQuery
+        .mockResolvedValueOnce({ searchPlayers: [player] })
+        .mockResolvedValueOnce({ listGames: games })
+      const result = await showPlayer(['-u', player.username])
+      expect(result).toMatchObject({ games })
+      const rawOutput = stripAnsi(applyFormaters(result).join('\n'))
+      expect(rawOutput).toContain(`total games:    4`)
+      expect(rawOutput).toContain(`owned games:    3`)
+      expect(rawOutput).toContain(`invited games:  1`)
     })
-    expect(
-      stripAnsi(
+
+    it('handles booleans, provider, admin, email', async () => {
+      mockQuery
+        .mockResolvedValueOnce({
+          searchPlayers: [
+            {
+              ...player,
+              isAdmin: true,
+              termsAccepted: false,
+              playing: true,
+              provider: 'github',
+              email: undefined
+            }
+          ]
+        })
+        .mockResolvedValueOnce({ listGames: [] })
+      const rawOutput = stripAnsi(
         applyFormaters(await showPlayer(['-u', player.username])).join('\n')
       )
-    ).toContain(`provider:       github`)
-  })
-
-  it('handles provider', async () => {
-    mockQuery.mockResolvedValueOnce({
-      searchPlayers: [{ ...player, email: undefined }]
+      expect(rawOutput).toContain(`is playing:     true`)
+      expect(rawOutput).toContain(`terms accepted: false`)
+      expect(rawOutput).toContain(`provider:       github`)
+      expect(rawOutput).toContain(`email:          none`)
     })
-    expect(
-      stripAnsi(
-        applyFormaters(await showPlayer(['-u', player.username])).join('\n')
-      )
-    ).toContain(`email:          none`)
-  })
-
-  it('handles admins', async () => {
-    mockQuery.mockResolvedValueOnce({
-      searchPlayers: [{ ...player, isAdmin: true }]
-    })
-    expect(
-      stripAnsi(
-        applyFormaters(await showPlayer(['-u', player.username])).join('\n')
-      )
-    ).toContain(`id:             ${player.id}`)
   })
 })
