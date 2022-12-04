60781bc156b9ed85fb8b6a5ce4e2bee2b9dc521c
feugy <damien.feugas@gmail.com>
2022-12-04 18:39:28 +0100

    feat(draughts): select side before joining game

--- apps/games/draughts/logic/players.js
+++ apps/games/draughts/logic/players.js
@@ -2,13 +2,48 @@ import { buildCameraPosition } from '@tabulous/server/src/utils/index.js'
 
 import { blackId, cameraPositions, whiteId } from './constants.js'
 
-export function addPlayer(game, player) {
-  const { cameras, playerIds } = game
-  const rank = playerIds.length
+export function askForParameters({ game: { preferences } }) {
+  const usedValues = preferences.map(({ side }) => side)
+  return usedValues.length
+    ? null
+    : {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          side: {
+            type: 'string',
+            enum: [whiteId, blackId].filter(
+              value => !usedValues.includes(value)
+            ),
+            metadata: {
+              fr: {
+                name: 'Couleur',
+                [whiteId]: 'Blancs',
+                [blackId]: 'Noirs'
+              }
+            }
+          }
+        },
+        required: ['side']
+      }
+}
+
+export function addPlayer(game, player, parameters) {
+  const { cameras, preferences } = game
+  // use selected preferences, or look for the first player color, and choose the other one.
+  const side =
+    preferences.length === 2
+      ? preferences[0].side === 'white'
+        ? 'black'
+        : 'white'
+      : parameters.side
+  // stores preferences for the next player added.
+  preferences[preferences.length - 1].side = side
+  // set camera based on selected side.
   cameras.push(
     buildCameraPosition({
       playerId: player.id,
-      ...cameraPositions[rank === 1 ? whiteId : blackId]
+      ...cameraPositions[side]
     })
   )
   return game
